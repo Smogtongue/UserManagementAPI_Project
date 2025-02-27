55ee07c92f13c5f80efb08191697afa34afae1f7
Zee <smogtongue@gmail.com>
2025-02-27 09:35:19 -0800

    Added Swagger/Swashbuckle components.

--- UserManagementAPI/obj/Debug/net9.0/UserManagementAPI.AssemblyInfo.cs
+++ UserManagementAPI/obj/Debug/net9.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3dd9dea055308383077b3011c4c8ebdf26ea35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab05ed1d24fd422849bbfead3433ec2040b90e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
